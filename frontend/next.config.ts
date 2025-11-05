36ae6e8ded17cacc0fa23b42508ba5ecb2fb98cb
Lê Thành Vinh <lethanhvinh@Les-MacBook-Pro.local>
2025-11-05 20:38:46 +0700

    update1

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export", // ⚡ Giúp tạo thư mục 'out' để Netlify có thể deploy
+  images: {
+    unoptimized: true, // ⚡ Bắt buộc khi dùng output: 'export'
+  },
 };
 
 export default nextConfig;
