d751195d48795ec1096a8b389ee8cef78dcc5cb6
Lê Thành Vinh <lethanhvinh@192.168.1.11.non-exists.ptr.local>
2025-11-06 17:03:52 +0700

    update thêm About

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 // src/app/page.tsx
-import HeroSection from "@/components/home/Herosection";
-import ProcessSection from "@/components/home/Processsection"; // 1. Import
+import HeroSection from "@/components/home/herosection";
+import ProcessSection from "@/components/home/processsection"; // 1. Import
 import ProductList from "@/components/home/productlist";
+import About from "@/components/home/about";
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
       <ProductList />
       <ProcessSection /> {/* 2. Thêm vào đây */}
+      <About /> {/* 3. Thêm vào đây */}
       {/* Các section khác ... */}
     </main>
   );
